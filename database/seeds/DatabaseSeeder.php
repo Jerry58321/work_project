5bd0412cc742a169215e0ac2499e668609e5f882
JerryLin <zey58321@gmail.com>
2018-08-15 19:56:22 +0800

    建立 seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table("users")->truncate();
+        $faker = Faker::create("zh_TW");
+        foreach (range(1,100) as $index) {
+            DB::table("users")
+              ->insert([
+                  'name' => $faker->name,
+                  'email' => $faker->email,
+              ]);
+        }
     }
 }
